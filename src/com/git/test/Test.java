942f4ffe821a6c17ff9747d5bfc45542c6a0911c
zhaopeng <497373442@qq.com>
2018-04-04 14:12:19 +0800

    commit 到本地仓库

--- src/com/git/test/Test.java
+++ src/com/git/test/Test.java
@@ -1,10 +1,16 @@
 package com.git.test;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Test {
 
+	public void toFormatDate(long time){
+		String d = new Date(time).toLocaleString();
+		System.out.println(d);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List list = new ArrayList();
